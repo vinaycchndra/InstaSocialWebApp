11e9eb6f7bf42117709a0ef23c7163d3ff09d47d
Vinay <Vinay@example.com>
2023-07-27 00:15:15 +0530

    cleaned some else statements

--- InstaService/views.py
+++ InstaService/views.py
@@ -21,13 +21,11 @@ class InstaPost(APIView):
     def post(self, request):
         data = request.data.copy()
         data['posted_by'] = request.user.id
-
         serializer = CreatePostSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response({'msg': 'Post created successfully !!!', 'data': serializer.data, 'error_msg': {}},
                             status=status.HTTP_201_CREATED)
-
         return get_errors(serializer)
 
     def patch(self, request, pk):
@@ -39,19 +37,16 @@ class InstaPost(APIView):
                     serializer.save()
                     return Response({'data': serializer.data, 'msg': 'Ok', 'error_msg': {}}, status=status.HTTP_200_OK)
                 return get_errors(serializer)
-            else:
-                return Response({'data': {}, 'msg': 'You do not have permission to edit this post', 'error_msg': {}},
-                                status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({'data': {}, 'msg': 'Not Found !', 'error_msg': {}}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'data': {}, 'msg': 'You do not have permission to edit this post', 'error_msg': {}},
+                            status=status.HTTP_400_BAD_REQUEST)
+        return Response({'data': {}, 'msg': 'Not Found !', 'error_msg': {}}, status=status.HTTP_404_NOT_FOUND)
 
     def get(self, request, pk):
         post_obj = self.get_object(pk)
         if post_obj:
             serializer = CreatePostSerializer(post_obj)
             return Response({'data': serializer.data, 'msg': 'Ok', 'error_msg': {}}, status=status.HTTP_200_OK)
-        else:
-            return Response({'data': {}, 'msg': 'Post does not exist', 'error_msg': {}}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'data': {}, 'msg': 'Post does not exist', 'error_msg': {}}, status=status.HTTP_404_NOT_FOUND)
 
     def delete(self, request, pk):
         post_obj = self.get_object(pk)
@@ -60,12 +55,10 @@ class InstaPost(APIView):
                 post_obj.delete()
                 return Response({'data': {}, 'msg': 'Deleted Successfully !', 'error_msg': {}},
                                 status=status.HTTP_204_NO_CONTENT)
-
             return Response({'data': {}, 'msg': 'You do not have permission to edit this post', 'error_msg': {}},
                             status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({'data': {}, 'msg': 'Post does not exist', 'error_msg': {}},
-                            status=status.HTTP_404_NOT_FOUND)
+        return Response({'data': {}, 'msg': 'Post does not exist', 'error_msg': {}},
+                        status=status.HTTP_404_NOT_FOUND)
 
     def get_object(self, pk):
         try:
@@ -92,20 +85,18 @@ class FollowUserView(APIView):
                 return Response({'msg': 'You have unfollowed {}!!!'.format(followed.get_full_name()),
                                  'data': {}, 'error_msg': {}},
                                 status=status.HTTP_204_NO_CONTENT)
-            else:
-                data = request.data.copy()
-                data['followed'] = followed.id
-                data['followe_by'] = request.user.id
-                serializer = FollowerSerializer(data=data)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response({'msg': 'You  successfully started following {}!!!'.format(followed.get_full_name()),
-                                     'data': serializer.data, 'error_msg': {}}, status=status.HTTP_201_CREATED)
-                return get_errors(serializer)
 
-        else:
-            return Response({'data': {}, 'msg': 'User does not exist', 'error_msg': {}},
-                            status=status.HTTP_404_NOT_FOUND)
+            data = request.data.copy()
+            data['followed'] = followed.id
+            data['followe_by'] = request.user.id
+            serializer = FollowerSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'msg': 'You  successfully started following {}!!!'.format(followed.get_full_name()),
+                                 'data': serializer.data, 'error_msg': {}}, status=status.HTTP_201_CREATED)
+            return get_errors(serializer)
+        return Response({'data': {}, 'msg': 'User does not exist', 'error_msg': {}},
+                        status=status.HTTP_404_NOT_FOUND)
 
     def get_user(self, pk):
         try:
